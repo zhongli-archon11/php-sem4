80973aeff8431a95b8e946e0dc6a9cca09d13ca2
Rex Lapis <91016552+zhongli-archon11@users.noreply.github.com>
2022-08-31 21:23:48 +0530

    weather in php

--- weather.php
+++ weather.php
@@ -1,11 +1,12 @@
 <?php
-	$url = "https://api.openweathermap.org/data/2.5/weather?lat=26.1425&lon=91.7889}&appid=0d64329ca82e10dd83024812dc1c9d8e";
+	$url = "https://api.openweathermap.org/data/2.5/weather?q=dispur&appid=896a8cbed5c40502dcb134637c775148";
 	$json = file_get_contents($url);
 	$json_data = json_decode($json);
-	
-	echo "<pre>";
-	print_r($json_data);
-	echo "</pre>";
 
-	#echo $json_data->weather->main;
+    echo "Date: " . date("d/m/Y") . "<br>";
+
+    echo $json_data->weather->main;
+    echo "Location(lon, lat)= (".$json_data->coord->lon.", ".$json_data->coord->lat.")";
+    $cels=($json_data->main->temp- 32)*5/9;
+    echo "<br>Temperature= ".$json_data->main->temp."&#8457;/".round($cels,2)."&#8451;<br>";
 ?>
